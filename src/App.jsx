3f2c668349cec32cc53d97599b1e83ee8f1900b1
Robin Vikström <robin.m.e.vikstrom@gmail.com>
2025-09-25 11:25:28 +0200

    connection between frontend & components fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
-import { BrowserRouter } from 'react-router-dom';
-import AppProvider from './providers/AppProvider.jsx';
-import PageShell from './templates/PageShell.template.jsx';
-import Approutes from './routes/AppRoutes.jsx';
+//import { BrowserRouter } from 'react-router-dom';
+//import AppProvider from './providers/AppProvider.jsx';
+//import PageShell from './templates/PageShell.template.jsx';
+//import Approutes from './routes/AppRoutes.jsx';
+import { Button } from "@ck/ck-components";
+import { GameBoard } from "@ck/ck-components";
 
-const App = () => (
-	<AppProvider>
-		<BrowserRouter>
-			<PageShell>
-				<AppRoutes />
-			</PageShell>
-		</BrowserRouter>
-	</AppProvider>
-);
 
+const App = () => {
+	return (
+		<div>
+		<Button />
+		<GameBoard />
+		</div>
+	 //<AppProvider>
+	//	<BrowserRouter>
+	//		<PageShell>
+	//			<AppRoutes />
+	//		</PageShell>
+	//	</BrowserRouter>
+	//</AppProvider>
+	);
+}; 
 export default App;
