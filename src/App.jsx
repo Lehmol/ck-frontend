0e02412161fc0c39136d0744c0a680e6da0345d4
Robin Vikström <robin.m.e.vikstrom@gmail.com>
2025-09-25 11:43:55 +0200

    changes made

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,9 @@
-<<<<<<< HEAD
 //import { BrowserRouter } from 'react-router-dom';
 //import AppProvider from './providers/AppProvider.jsx';
 //import PageShell from './templates/PageShell.template.jsx';
 //import Approutes from './routes/AppRoutes.jsx';
 import { Button } from "@ck/ck-components";
 import { GameBoard } from "@ck/ck-components";
-
-=======
-import React from "react";
-
-function App() {
-	return(
-		<div>Frontend repository for Gomoku game</div>
-	);
-};
->>>>>>> afda427e918eb02d09fee888424c8d6c24ac6c7e
 
 const App = () => {
 	return (
