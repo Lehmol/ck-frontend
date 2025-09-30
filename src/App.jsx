2f7b137fd8ce377e1ea6146dee4dd0da6e451c6f
mariaMerethe <borglundmaria@gmail.com>
2025-09-30 11:37:44 +0200

    created a ConfigProvider.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //import { BrowserRouter } from 'react-router-dom';
 //import AppProvider from './providers/AppProvider.jsx';
 //import PageShell from './templates/PageShell.template.jsx';
@@ -6,15 +5,6 @@
 import { Button } from "@ck/ck-components";
 import { GameBoard } from "@ck/ck-components";
 
-=======
-import React from "react";
-
-function App() {
-	return(
-		<div>Frontend repository for Gomoku game</div>
-	);
-};
->>>>>>> afda427e918eb02d09fee888424c8d6c24ac6c7e
 
 const App = () => {
 	return (
