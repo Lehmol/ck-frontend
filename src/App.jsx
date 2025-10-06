699dde8f54315e027e158778a92ea72a971f9557
Lehmol <chalottelinnea@hotmail.com>
2025-10-06 10:58:38 +0200

    deleted import of Piece and Button

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,15 @@
 //import AppProvider from './providers/AppProvider.jsx';
 //import PageShell from './templates/PageShell.template.jsx';
 //import Approutes from './routes/AppRoutes.jsx';
-import { Button } from "@ck/ck-components";
 import { GameBoard } from "@ck/ck-components";
 import { Background } from "@ck/ck-components";
 import { Players } from "@ck/ck-components";
-import { Piece } from "@ck/ck-components";
 
 const App = () => {
   return (
     <Background>
       <GameBoard />
-      <Button />
+      <Players />
     </Background>
   );
 };
